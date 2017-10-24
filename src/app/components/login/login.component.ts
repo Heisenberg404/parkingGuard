4de06493eb63e5890c93d3a46a1ac54a885d5993
alexandrardila <alexandrita1106@hotmail.com>
2017-10-24 18:09:13 -0500

    aleca ch

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,6 +6,8 @@ import { ParkingApiService} from '../../services/parkingApi.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent implements OnInit {
   user: any = {
     id : '',
@@ -13,6 +15,8 @@ export class LoginComponent implements OnInit {
     pass : '',
     operacion : false
   };
+
+  msjError = '';
 
   loggedUSer: any = null;
 
@@ -25,11 +29,26 @@ export class LoginComponent implements OnInit {
     this._parkingApiService.getUsers(this.user).subscribe();
   }
   checkUser () {
+    this.validateFields ();
+    if (this.msjError='') {
     this._parkingApiService.checkUser(this.user).subscribe(result => {
       console.log('resultado: ' + result);
       this.loggedUSer = result;
       console.log('el usuario en sesion es: ' + this.loggedUSer.username + 'con id: ' + this.loggedUSer.id);
     });
+    }else {
+      console.log(this.msjError);
+    }
+  }
+
+  validateFields () {
+    if (/^[0-9]+$/.test(this.user.username)) {
+      this.msjError = 'el usuario debe ser solo letras';
+    } else if (!/^[0-9]+$/.test(this.user.pass)) {
+      this.msjError = 'f'
+    } else {
+      this.msjError = '';
+    }
   }
 
 }
